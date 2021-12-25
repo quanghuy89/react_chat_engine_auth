7c6270b7e6ca6c757af08f9f5d20f22cd83336a1
Dinh Quang Huy <narutadanbamba@gmail.com>
2021-12-25 21:06:09 +0700

    Updates

---
+++ precache-manifest.a5425be3c59d6c9335b92f920646fe95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7943f45b33bc186dd18722fec0a51006",
+    "url": "/react-chat-app/index.html"
+  },
+  {
+    "revision": "2a4487b17ca9e012a118",
+    "url": "/react-chat-app/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "e0dd44e2427b51f5c7f1",
+    "url": "/react-chat-app/static/js/2.d38e0588.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/react-chat-app/static/js/2.d38e0588.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4487b17ca9e012a118",
+    "url": "/react-chat-app/static/js/main.63d753eb.chunk.js"
+  },
+  {
+    "revision": "f928c972c4b2a74c376d",
+    "url": "/react-chat-app/static/js/runtime-main.47cfda1f.js"
+  }
+]);
